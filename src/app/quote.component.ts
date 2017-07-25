a08d72829d2c5321fb63b99e8fe47db99d54322e
Minh Vu <mdvu15@earlham.edu>
2017-07-24 23:07:06 -0400

    set up

--- src/app/quote.component.ts
+++ src/app/quote.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-	selector: 'quote'
-	
+	selector: 'quote',
+	templateUrl: './quote.component.html',
+	styleUrls: ['./quote.component.css']
 })
