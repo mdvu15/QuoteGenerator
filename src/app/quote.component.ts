f53ebfdbcb2e47f11041076b1dd4a16d38ad0b36
Minh Vu <mdvu15@earlham.edu>
2017-07-25 10:02:54 -0400

    quote service

--- src/app/quote.component.ts
+++ src/app/quote.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import { QuoteService } from './quote.service'
 import { Quote } from './quote';
 
 @Component({
@@ -8,10 +9,12 @@ import { Quote } from './quote';
 	//styleUrls: ['./quote.component.css']
 })
 
-export class QuoteComponent {
-    newQuote = new Quote();
+export class QuoteComponent implements OnInit {
+    quote: Quote;
+
+    constructor(private quoteService: QuoteService) {}
 
     getQuote(): void {
-        this.newQuote.content = "Deep";
+        
     }
 }
