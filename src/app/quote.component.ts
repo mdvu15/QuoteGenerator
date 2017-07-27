30b1d448e4b525da628352a65411e987c82c8c36
Minh Vu <mdvu15@earlham.edu>
2017-07-27 15:13:04 -0400

    add twitter link - not test

--- src/app/quote.component.ts
+++ src/app/quote.component.ts
@@ -1,20 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
-import { QuoteService } from './quote.service'
+import { WindowService } from './window.service';
+import { QuoteService } from './quote.service';
 import { Quote } from './quote';
 
 @Component({
 	selector: 'quote',
 	templateUrl: './quote.component.html',
 	//styleUrls: ['./quote.component.css']
+    providers: [WindowService]
 })
 
 export class QuoteComponent implements OnInit {
-    quote: Quote;
-    constructor(private quoteService: QuoteService) {}
+    public quote: Quote;
+    constructor(private quoteService: QuoteService,
+                private nativeWindow: WindowService
+                ) {}
 
     getQuote(): void {
         this.quoteService.getQuote().then(quote => this.quote = quote);
+        if (this.quote.quoteAuthor === "") {
+            this.quote.quoteAuthor = "Unknown";
+        }
+    } 
+
+    shareTwitter(): void {
+        this.nativeWindow.getWindow()
+        .open(`https://twitter.com/intent/tweet?text=${this.quote.quoteText}-${this.quote.quoteAuthor}`);
     }
 
     ngOnInit(): void {
